cbc0bb556241b6716d0937cdf3745826c0d807e0
SashaAndPlay <SashaAndPlay@gmail.com>
2020-11-02 22:03:21 +1000

    готов
    выполнил задание

--- main.py
+++ main.py
@@ -4,26 +4,24 @@ W = 800
 H = 600
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+block = 0
 a = "Всем привет"
 b = "Задание на урок"
 
-square = pygame.display.set_mode((50, 50))
-square.fill(RED)
+
 
 pygame.init()
 pygame.display.set_caption('Задание на урок')
 pygame.mouse.set_visible(True)
-screen = pygame.display.set_mode((W, H), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((W, H))
 screen.fill(BLUE)
 
+red = pygame.Surface((100, 100))
+red_rect = red.get_rect(center=(400, 150))
+
 font1 = pygame.font.SysFont('Arial', 40, True, False)
-font2 = pygame.font.SysFont('Arial', 25, True, False)
-screen.blit(font1.render(a, True, BLUE), (50, 50))
-
-
-
-text1 = pygame.Surface((W - 30, font1.get_height()))
-text1_rect = text1.get_rect(center=(W // 2, H - 30))
+font2 = pygame.font.SysFont('Arial', 25, False, False)
 
 
 run = True
@@ -35,3 +33,9 @@ while run:
             if e.key == pygame.K_ESCAPE:
                 run = False
 
+    if block == 0:            
+        screen.blit(red, red_rect)
+        red.fill(RED)
+        screen.blit(font1.render(a, True, RED), (300, 300))
+        screen.blit(font2.render(b, True, WHITE), (320, 400))
+        pygame.display.update()
